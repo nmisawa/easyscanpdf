b20ef7aafebcc50c1673e31672aa70569d95d9b6
EasyScanPDF Contributors <opensource@example.com>
2026-03-21 11:20:23 +0900

    Initial Commit

---
+++ monascan/monascanApp.swift
@@ -0,0 +1,17 @@
+//
+//  monascanApp.swift
+//  monascan
+//
+//  Created by developer on 2026/03/21.
+//
+
+import SwiftUI
+
+@main
+struct monascanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
